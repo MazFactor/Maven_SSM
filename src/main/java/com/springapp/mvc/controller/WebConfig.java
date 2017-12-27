de47d7e689c37a16080f97d7465543fbca0cfd33
MazFactor <33100949+MazFactor@users.noreply.github.com>
2017-12-28 06:57:08 +0800

    Add one controller and fix index.jsp
    1. Add controller WebConfig.java to process static resources.
    2. Remove "height: 100%" from .column-all in index.jsp, since it'll make the footer not stay at the bottom of the page.

---
+++ src/main/java/com/springapp/mvc/controller/WebConfig.java
@@ -0,0 +1,16 @@
+package com.springapp.mvc.controller;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+}
